288d26c419bf897daa34461b45a237ea896d188e
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-02-27 20:03:30 +0200

    update solution

--- neetCode.cs
+++ neetCode.cs
@@ -1,51 +1,17 @@
 public class Solution {
-    // Input: nums = [1,1,2,3,4]
-    // Output: [1,2,3,4]
-    // Explanation: You should return k = 4 as we have four unique elements. 
-
-    // The first k elements of nums must contain the unique values.
-    // what does the above actually mean
-    // 
-    public int RemoveDuplicates(int[] nums) {
-        // loop through nums
-        // have 2 pointers
-        // our right pointer is going traverse every num in array this can be i
-        // our left pointer is going to keep the index of our last unique number
-        // this can be k
-
-        // we know that our first num is unique so lets start at 1
-        int k = 1;
-
-        for(var i = 1; i < nums.Length; i++){
-            // so we need to compare our current number
-            var currentNum = nums[i];
-            // to our previous num
-            var previousNum = nums[i - 1];
-
-            // if our previousNum is the same as our current number
-            // then we don't increment k and keep looping to the next number
-
-            // if our previousNum is different than our current number
-            if(currentNum != previousNum){
-                // then we are going to place our current number with our right pointer
-                // [1,1,2,3,4]
-                //  k ^
-                nums[k] = currentNum;
-                k++;
-                // at the position our left pointer is at
-                // and then we are going to increment left pointer by one
-                // then keep traversing the array with both pointers
-            }
-        }
-
-        return k;
+    // Min Stack
+    // work through the solutions below with white board and check off on notion
+    // then create a new class called min stack and solve the above
+ 
+    // Valid Parenthesis:
+    public bool IsValid(string s) {
     }
 
 
+    public int RemoveDuplicates(int[] nums) {
+    }
 
-    // GetConcatenation of arrays
-    // from now on we are going to use white board to think first
-    // then we can code the solution starting from this method below:
+
     public int[] GetConcatenation(int[] nums) {
         int n = nums.Length;
         int k = n;
@@ -60,22 +26,4 @@ public class Solution {
 
         return ans;
     }
-
- 
- 
-
-
-    // Valid Parenthesis:
-    public bool IsValid(string s) {
-    }
-
-
-
-
-
-
-
-
-
-
 }
