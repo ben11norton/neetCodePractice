da55f6f01a294db4993f96c3b45365804785992e
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-03-03 08:14:22 +0200

    remove duplicates

--- neetCode.cs
+++ neetCode.cs
@@ -1,26 +1,11 @@
 public class Arrays {
 
+    // 1. solve again on white board
+    // then submit the code
+    public int RemoveDuplicates(int[] nums) {
+    }
+
     public int[] TwoSum(int[] nums, int target) {
-        // hasMap to store our current number as the key
-        // and the index as the value (because we are interested in the indeces as the return values)
-        var hashMap = new Dictionary<int, int>();
-
-        for(var i = 0; i < nums.Length; i++){
-            var currentIndex = i;
-            var currentNum = nums[currentIndex];
-
-            var difference = target - currentNum;
-            // check if our hashMap has the difference (the number that sums up to target with our current number)
-            var containsMissingNumber = hashMap.ContainsKey(difference);
-            if(containsMissingNumber){
-                return [currentIndex, hashMap[difference]];
-
-            } else{
-                hashMap[currentNum] = currentIndex;
-            }
-        }
-
-        return []; // returning and empty array incase it doesn't find a match
     }
 
     public int[] GetConcatenation(int[] nums) {
