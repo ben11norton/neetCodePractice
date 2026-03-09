00339f4d0e51eb2734c5e8af543f76dcb7d73500
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-03-09 17:36:45 +0200

    stack practice - valid parentheses

--- neetCode.cs
+++ neetCode.cs
@@ -19,4 +19,46 @@ public class ArrayTwoPointers {
 
 
 public class Stacks {
+
+
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+
+        var matchingMap = new Dictionary<char, char>{
+            {'(', ')'},
+            {'[', ']'},
+            {'{', '}'}
+        };
+
+        for(var i = 0; i < s.Length; i++){
+            var currentBracket = s[i];
+
+            if(matchingMap.ContainsKey(currentBracket)){
+                stack.Push(currentBracket);
+
+            } else{
+
+                if(stack.Count == 0){
+                    return false;
+
+                } else{
+                    var lastAddedOpeningBracket = stack.Pop();
+                    var matchingBracket = matchingMap[lastAddedOpeningBracket] == currentBracket;
+                    if(!matchingBracket){
+                        return false;
+                    }
+                }
+            }
+        }
+
+        var stackIsEmpty = stack.Count == 0;
+        return stackIsEmpty;
+        // this solution is O(n)
+        // we loop through each character in the string only once so grows linarly with length of string
+        // using .Pop or .Push operations are O(1) operations
+        // use of dictionary is also O(1) look up
+        // so our time complexity is O(n)
+        // so n * O(1) = O(n)
+    }
+
 }
