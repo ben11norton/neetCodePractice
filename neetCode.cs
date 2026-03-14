c28ea3e7e132409b22d698e9ec72ffbfb10a92a8
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-03-14 15:13:48 +0200

    stack recap

--- neetCode.cs
+++ neetCode.cs
@@ -13,15 +13,6 @@ public class Arrays {
 public class HashingArrays {
 
     public bool ContainsDuplicate(int[] nums) {
-        var map = new Dictionary<int, int>();
-        for(var i = 0; i < nums.Length; i++){
-            if(!map.ContainsKey(nums[i])){
-                map[nums[i]] = i;
-            } else{
-                return true;
-            }
-        }
-        return false;
     }
 }
 
@@ -35,42 +26,38 @@ public class Stacks {
 
     public bool IsValid(string s) {
         var stack = new Stack<char>();
-
-        var matchingMap = new Dictionary<char, char>{
+        var map = new Dictionary<char, char>(){
             {'(', ')'},
             {'[', ']'},
             {'{', '}'}
         };
-
         for(var i = 0; i < s.Length; i++){
             var currentBracket = s[i];
-
-            if(matchingMap.ContainsKey(currentBracket)){
+            if(map.ContainsKey(currentBracket)){
                 stack.Push(currentBracket);
-
             } else{
-
                 if(stack.Count == 0){
                     return false;
-
-                } else{
-                    var lastAddedOpeningBracket = stack.Pop();
-                    var matchingBracket = matchingMap[lastAddedOpeningBracket] == currentBracket;
-                    if(!matchingBracket){
-                        return false;
-                    }
+                }
+                var lastAddedOpeningBracket = stack.Pop();
+                var closingBracketToMatch = map[lastAddedOpeningBracket];
+                if(currentBracket != closingBracketToMatch){
+                    return false;
                 }
             }
         }
-
         var stackIsEmpty = stack.Count == 0;
         return stackIsEmpty;
-        // this solution is O(n)
-        // we loop through each character in the string only once so grows linarly with length of string
-        // using .Pop or .Push operations are O(1) operations
-        // use of dictionary is also O(1) look up
-        // so our time complexity is O(n)
-        // so n * O(1) = O(n)
     }
 
 }
+
+
+
+
+
+
+
+
+
+
--- program.cs
+++ program.cs
@@ -1,31 +1,3 @@
 public class Program{
 
-    public int[] TopKFrequent(int[] nums, int k) {
-        var map = new Dictionary<int, int>();
-        for(var i = 0; i < nums.Length; i++){
-            if(!map.ContainsKey(nums[i]){
-                map[nums[i]] = 1;
-            } else{
-                map[nums[i]]++;
-            }
-        }
-
-        var topKNums = new int[k];
-        var topKValues = new int[k];
-        foreach(var (key, value) in map){
-            var currentKey = key;
-            var currentValue = value;
-            for(var i = 0; i < topKNums.Length; i++){
-                if(currentValue > topKValues[i]){
-                    var tempValue = topKValues[i];
-                    topKValues[i] = currentValue;
-                    currentValue = tempValue;
-                    var tempKey = topKNums[i];
-                    topKNums[i] = currentKey;
-                    currentKey = tempKey;
-                }
-            }
-        }
-        return topKNums;
-    }
 }
