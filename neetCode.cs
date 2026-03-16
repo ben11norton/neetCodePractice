003a007b13ae5143fb116c9e259e29ada73734f8
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-03-16 06:53:31 +0200

    remove duplicate

--- neetCode.cs
+++ neetCode.cs
@@ -3,6 +3,17 @@ public class Arrays {
     public int RemoveDuplicates(int[] nums) {
     }
 
+    public int RemoveElement(int[] nums, int val) {
+        var k = 0;
+        for(var i = 0; i < nums.Length; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
     public int[] TwoSum(int[] nums, int target) {
     }
 
@@ -10,29 +21,12 @@ public class Arrays {
     }
 
     public int[] RunningSum(int[] nums) {
-        var n = nums.Length;
-        var runningSum = new int[n];
-        var sum = 0;
-        for(var i = 0; i < nums.Length; i++){
-            sum += nums[i];
-            runningSum[i] = sum;
-        }
-        return runningSum;
     }
 }
 
 public class HashingArrays {
 
     public bool ContainsDuplicate(int[] nums) {
-        var map = new Dictionary<int, int>();
-        for(var i = 0; i < nums.Length; i++){
-            if(map.ContainsKey(nums[i])){
-                return true;
-            } else{
-                map[nums[i]] = i;
-            }
-        }
-        return false;
     }
 }
 
@@ -45,29 +39,6 @@ public class Stacks {
 
 
     public bool IsValid(string s) {
-        var stack = new Stack<char>();
-        var map = new Dictionary<char, char>(){
-            {'(', ')'},
-            {'[', ']'},
-            {'{', '}'}
-        };
-        for(var i = 0; i < s.Length; i++){
-            var currentBracket = s[i];
-            if(map.ContainsKey(currentBracket)){
-                stack.Push(currentBracket);
-            } else{
-                if(stack.Count == 0){
-                    return false;
-                }
-                var lastAddedOpeningBracket = stack.Pop();
-                var closingBracketToMatch = map[lastAddedOpeningBracket];
-                if(currentBracket != closingBracketToMatch){
-                    return false;
-                }
-            }
-        }
-        var stackIsEmpty = stack.Count == 0;
-        return stackIsEmpty;
     }
 
 }
