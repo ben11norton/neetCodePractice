92b2a3521c97ee07423b980dc7e495a6f415834d
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-03-17 06:40:53 +0200

    solved baseball game

--- neetCode.cs
+++ neetCode.cs
@@ -4,14 +4,6 @@ public class Arrays {
     }
 
     public int RemoveElement(int[] nums, int val) {
-        var k = 0;
-        for(var i = 0; i < nums.Length; i++){
-            if(nums[i] != val){
-                nums[k] = nums[i];
-                k++;
-            }
-        }
-        return k;
     }
 
     public int[] TwoSum(int[] nums, int target) {
@@ -37,8 +29,37 @@ public class ArrayTwoPointers {
 
 public class Stacks {
 
-
     public bool IsValid(string s) {
+    }
+
+    public int CalPoints(string[] operations) {
+        var stack = new Stack<int>();
+        for(var i = 0; i < operations.Length; i++){
+            if(int.TryParse(operations[i], out int num)){
+                stack.Push(num);
+            } else if(operations[i] == "C"){
+                stack.Pop();
+            } else if(operations[i] == "D"){
+                var prevScore = stack.Peek();
+                var doubleScore  = 2 * prevScore;
+                stack.Push(doubleScore);
+            } else if(operations[i] == "+"){
+                var top = stack.Pop();
+                var secondTop = stack.Peek();
+                var sumOfTop2 = top + secondTop;
+                stack.Push(top);
+                stack.Push(sumOfTop2);
+            }
+        }
+        var sum = 0;
+        if(stack.Count == 0){
+            return sum;
+        } else{
+            foreach(var num in stack){
+                sum += num;
+            }
+            return sum;
+        }
     }
 
 }
