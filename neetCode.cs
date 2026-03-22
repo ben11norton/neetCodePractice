87b3a6942d819c1977c5d85481dce6c45676bb23
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-03-22 09:53:51 +0200

    Remove Element review

--- neetCode.cs
+++ neetCode.cs
@@ -1,64 +1,83 @@
-public class Arrays {
+public class Arrays
+{
 
-    public int RemoveDuplicates(int[] nums) {
+    public int RemoveDuplicates(int[] nums)
+    {
     }
 
-    public int RemoveElement(int[] nums, int val) {
-    }
-
-    public int[] TwoSum(int[] nums, int target) {
-    }
-
-    public int[] GetConcatenation(int[] nums) {
-    }
-
-    public int[] RunningSum(int[] nums) {
-    }
-
-    public int MaximumWealth(int[][] accounts) {
-        var maxWealth = 0;
-        for(var i = 0; i < accounts.Length; i++){
-            var account = accounts[i];
-            var accountSum = 0;
-            for(var j = 0; j < account.Length; j++){
-                accountSum += account[j];
-                if(accountSum > maxWealth){
-                    maxWealth = accountSum;
-                }
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return maxWealth;
+        return k;
+    }
+
+    public int[] TwoSum(int[] nums, int target)
+    {
+    }
+
+    public int[] GetConcatenation(int[] nums)
+    {
+    }
+
+    public int[] RunningSum(int[] nums)
+    {
+    }
+
+    public int MaximumWealth(int[][] accounts)
+    {
     }
 }
 
-public class HashingArrays {
+public class HashingArrays
+{
 
-    public bool ContainsDuplicate(int[] nums) {
+    public bool ContainsDuplicate(int[] nums)
+    {
     }
 }
 
 
-public class ArrayTwoPointers {
+public class ArrayTwoPointers
+{
 }
 
 
-public class Stacks {
+public class Stacks
+{
 
-    public bool IsValid(string s) {
+    public bool IsValid(string s)
+    {
     }
 
-    public int CalPoints(string[] operations) {
+    public int CalPoints(string[] operations)
+    {
         var stack = new Stack<int>();
-        for(var i = 0; i < operations.Length; i++){
-            if(int.TryParse(operations[i], out int num)){
+        for (var i = 0; i < operations.Length; i++)
+        {
+            if (int.TryParse(operations[i], out int num))
+            {
                 stack.Push(num);
-            } else if(operations[i] == "C"){
+            }
+            else if (operations[i] == "C")
+            {
                 stack.Pop();
-            } else if(operations[i] == "D"){
+            }
+            else if (operations[i] == "D")
+            {
                 var prevScore = stack.Peek();
-                var doubleScore  = 2 * prevScore;
+                var doubleScore = 2 * prevScore;
                 stack.Push(doubleScore);
-            } else if(operations[i] == "+"){
+            }
+            else if (operations[i] == "+")
+            {
                 var top = stack.Pop();
                 var secondTop = stack.Peek();
                 var sumOfTop2 = top + secondTop;
@@ -67,10 +86,14 @@ public class Stacks {
             }
         }
         var sum = 0;
-        if(stack.Count == 0){
+        if (stack.Count == 0)
+        {
             return sum;
-        } else{
-            foreach(var num in stack){
+        }
+        else
+        {
+            foreach (var num in stack)
+            {
                 sum += num;
             }
             return sum;
