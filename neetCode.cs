c72d4dc9b33fa6b78116eed207025a41bbc97e94
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-03-26 06:45:09 +0200

    brute force submission for sorted Two Sum 2 pointers

--- neetCode.cs
+++ neetCode.cs
@@ -39,56 +39,35 @@ public class HashingArrays
 
     public bool IsAnagram(string s, string t)
     {
-        // time complexity is O(n)
-        // where n is the length of s and t
-        // we are iterating over each element of s and performing O(1) operations to build our map
-        // we then loop through each element of t and perform O(1) look up operations
-        // so total time becomes O(2n) which becomes O(n) as we are not concerned about constants
-        // total time is linear to the length of the strings
-        if (s.Length != t.Length)
-        {
-            return false;
-        }
-
-        var map = new Dictionary<char, int>();
-        for (var i = 0; i < s.Length; i++)
-        {
-            if (!map.ContainsKey(s[i]))
-            {
-                map[s[i]] = 1;
-            }
-            else
-            {
-                map[s[i]]++;
-            }
-        }
-
-        for (var i = 0; i < t.Length; i++)
-        {
-            if (map.ContainsKey(t[i]))
-            {
-                if (map[t[i]] > 0)
-                {
-                    map[t[i]]--;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
 
 
 public class ArrayTwoPointers
 {
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        // need to redo this one
+        // we got the correct answer but still is O(n sqaured in worst case)
+        // as basically doing a nested loop here
+        var k = 0;
+        for (var i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[k] == target - numbers[i])
+            {
+                return [k + 1, i + 1];
+            }
+            else
+            {
+                if (i == numbers.Length - 1 && k != numbers.Length - 2)
+                {
+                    k++;
+                    i = k;
+                }
+            }
+        }
+        return new int[0];
+    }
 }
 
 
