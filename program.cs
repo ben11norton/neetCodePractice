4b0d243cc9495af33bda1858f0fd47a389d5b5e0
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-04-26 17:48:58 +0200

    2 pointer review

--- program.cs
+++ program.cs
@@ -1,46 +1,15 @@
 public class Program
 {
-    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    public void ReverseString(char[] s)
     {
-        var map = new Dictionary<int, ListNode>();
-        var index = 0;
-        var current = head;
-        while (current != null)
+        var R = s.Length - 1;
+        for (var i = 0; i < s.Length / 2; i++)
         {
-            map[index] = current;
-            index++;
-            current = current.next;
+            var temp = s[i];
+            s[i] = s[R];
+            s[R] = temp;
+            R--;
         }
-        // map [0: 1]
-        var listLength = index; // 1
-        var nodeToRemoveIndex = listLength - n;// 0
-        var previousNodeIndex = nodeToRemoveIndex - 1;
-        if (previousNodeIndex >= 0)
-        {
-            var previousNode = map[nodeToRemoveIndex - 1];
-            if (nodeToRemoveIndex + 1 > listLength - 1)
-            {
-                previousNode.next = null;
-            }
-            else
-            {
-                var nodeAfter = map[nodeToRemoveIndex + 1];
-                previousNode.next = nodeAfter;
-            }
-        }
-        else
-        {
-            if (nodeToRemoveIndex + 1 <= listLength - 1)
-            {
-                var nodeAfter = map[nodeToRemoveIndex + 1];
-                head = nodeAfter;
-            }
-            else
-            {
-                return null;
-            }
-        }
-        return head;
     }
 }
 
