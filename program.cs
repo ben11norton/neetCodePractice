28e87bd80b033b01702981c5b07ee831c55b3a8c
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-04-26 17:14:34 +0200

    sliding window review

--- program.cs
+++ program.cs
@@ -1,15 +1,21 @@
 public class Program
 {
-    public void ReverseString(char[] s)
-    {
-        int R = s.Length - 1;
-        for (var i = 0; i < s.Length / 2; i++)
-        {
-            char temp = s[i];
-            s[i] = s[R];
-            s[R] = temp;
-            R--;
+    public double FindMaxAverage(int[] nums, int k) {
+        var windowSum = 0;
+        double maxAvg = 0;
+        for(var i = 0; i < k; i++){
+            windowSum += nums[i];
         }
+        maxAvg = (double)windowSum / k;
+        for(var i = k; i < nums.Length; i++){
+            windowSum -= nums[i - k];
+            windowSum += nums[i]; 
+            var newAvg = (double)windowSum / k;
+            if(newAvg > maxAvg){
+                maxAvg = newAvg;
+            }
+        }
+        return maxAvg;
     }
 }
 
