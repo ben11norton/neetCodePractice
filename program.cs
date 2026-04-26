59e8549e1fb35c3172c233863c59aa41a1b3bec5
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-04-26 17:43:35 +0200

    linked list hell

--- program.cs
+++ program.cs
@@ -1,21 +1,46 @@
 public class Program
 {
-    public double FindMaxAverage(int[] nums, int k) {
-        var windowSum = 0;
-        double maxAvg = 0;
-        for(var i = 0; i < k; i++){
-            windowSum += nums[i];
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        var map = new Dictionary<int, ListNode>();
+        var index = 0;
+        var current = head;
+        while (current != null)
+        {
+            map[index] = current;
+            index++;
+            current = current.next;
         }
-        maxAvg = (double)windowSum / k;
-        for(var i = k; i < nums.Length; i++){
-            windowSum -= nums[i - k];
-            windowSum += nums[i]; 
-            var newAvg = (double)windowSum / k;
-            if(newAvg > maxAvg){
-                maxAvg = newAvg;
+        // map [0: 1]
+        var listLength = index; // 1
+        var nodeToRemoveIndex = listLength - n;// 0
+        var previousNodeIndex = nodeToRemoveIndex - 1;
+        if (previousNodeIndex >= 0)
+        {
+            var previousNode = map[nodeToRemoveIndex - 1];
+            if (nodeToRemoveIndex + 1 > listLength - 1)
+            {
+                previousNode.next = null;
+            }
+            else
+            {
+                var nodeAfter = map[nodeToRemoveIndex + 1];
+                previousNode.next = nodeAfter;
             }
         }
-        return maxAvg;
+        else
+        {
+            if (nodeToRemoveIndex + 1 <= listLength - 1)
+            {
+                var nodeAfter = map[nodeToRemoveIndex + 1];
+                head = nodeAfter;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        return head;
     }
 }
 
