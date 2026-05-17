505f19db2887011cd75c40432d3ec6ceca9b1bee
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-05-17 16:47:06 +0200

    isomorphic check with maps

--- program.cs
+++ program.cs
@@ -1,5 +1,39 @@
 public class Program
 {
+    public bool IsIsomorphic(string s, string t)
+    {
+        if (s.Length == t.Length)
+        {
+            var mapS = new Dictionary<char, char>();
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (!mapS.ContainsKey(s[i]))
+                {
+                    mapS[s[i]] = t[i];
+                }
+            }
+            var mapT = new Dictionary<char, char>();
+            for (var i = 0; i < t.Length; i++)
+            {
+                if (!mapT.ContainsKey(t[i]))
+                {
+                    mapT[t[i]] = s[i];
+                }
+            }
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (mapS[s[i]] != t[i] || mapT[t[i]] != s[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
 
 
