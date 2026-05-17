6c2edc6455213247965f3917bce5b75710a74532
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-05-17 16:38:17 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,26 +1,5 @@
 public class Program
 {
-    public void Merge(int[] nums1, int m, int[] nums2, int n)
-    {
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-        while (j >= 0)
-        {
-            if (i >= 0 && nums1[i] > nums2[j])
-            {
-                nums1[k] = nums1[i];
-                nums1[i] = nums2[j];
-                i--;
-            }
-            else
-            {
-                nums1[k] = nums2[j];
-                j--;
-            }
-            k--;
-        }
-    }
 }
 
 
