aedd442f06c522da9f4722e7579ecf84aef42285
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-05-17 16:15:06 +0200

    make string great with string concatenation

--- program.cs
+++ program.cs
@@ -1,5 +1,28 @@
 public class Program
 {
+    public string MakeGood(string s)
+    {
+        var stack = new Stack<char>();
+        stack.Push(s[0]);
+        for (var i = 1; i < s.Length; i++)
+        {
+            if (stack.Count > 0 && char.ToLower(s[i]) == char.ToLower(stack.Peek()) && s[i] != stack.Peek())
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(s[i]);
+            }
+        }
+        int stackCount = stack.Count();
+        string newString = "";
+        for (var i = 0; i < stackCount; i++)
+        {
+            newString = stack.Pop() + newString;
+        }
+        return newString;
+    }
 }
 
 
