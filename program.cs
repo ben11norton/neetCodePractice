64d45b55523780b9658c7cdb302ddaed0274fac3
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-05-18 08:32:21 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,33 +1,5 @@
 public class Program
 {
-    public bool BackspaceCompare(string s, string t)
-    {
-        string newS = BackspaceString(s);
-        string newT = BackspaceString(t);
-        return newS == newT;
-    }
-
-    private string BackspaceString(string s)
-    {
-        int backspaceCount = 0;
-        string newS = "";
-        for (var i = s.Length - 1; i >= 0; i--)
-        {
-            if (s[i] == '#')
-            {
-                backspaceCount++;
-            }
-            else if (backspaceCount > 0)
-            {
-                backspaceCount--;
-            }
-            else
-            {
-                newS = s[i] + newS;
-            }
-        }
-        return newS;
-    }
 }
 
 
