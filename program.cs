da3910c1ccb0fb2857a057d872016241d6c80308
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-05-26 07:40:58 +0200

    move zeroes

--- program.cs
+++ program.cs
@@ -1,5 +1,31 @@
 public class Program
 {
+    public void MoveZeroes(int[] nums)
+    {
+        int k = 0;
+        bool zeroeSlot = false;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0 && zeroeSlot)
+            {
+                nums[k] = nums[i];
+                nums[i] = 0;
+                if (nums[k + 1] == 0)
+                {
+                    k = k + 1;
+                }
+                else
+                {
+                    k = i;
+                }
+            }
+            else if (nums[i] == 0 && !zeroeSlot)
+            {
+                zeroeSlot = true;
+                k = i;
+            }
+        }
+    }
 }
 
 
