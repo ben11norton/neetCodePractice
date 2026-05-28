3026e0e89623d9d44a472dfd9be7aee27cccc506
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-05-28 07:42:22 +0200

    crawler log folder - stack easy

--- program.cs
+++ program.cs
@@ -1,5 +1,21 @@
 public class Program
 {
+    public int MinOperations(string[] logs)
+    {
+        var stack = new Stack<string>();
+        for (var i = 0; i < logs.Length; i++)
+        {
+            if (logs[i] == "../" && stack.Count > 0)
+            {
+                stack.Pop();
+            }
+            else if (logs[i] != "./" && logs[i] != "../")
+            {
+                stack.Push(logs[i]);
+            }
+        }
+        return stack.Count;
+    }
 }
 
 
