cc0e06283e8824e55c6ee6264e9f7020ef4a2f33
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-08 07:45:29 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,26 +1,5 @@
 public class Program
 {
-    public int MaxScore(int[] cardPoints, int k)
-    {
-        int score = 0;
-        for (var i = 0; i < k; i++)
-        {
-            score += cardPoints[i];
-        }
-        int maxScore = score;
-        int pointer = 1;
-        while (pointer <= k)
-        {
-            score -= cardPoints[k - pointer];
-            score += cardPoints[cardPoints.Length - pointer];
-            pointer++;
-            if (score > maxScore)
-            {
-                maxScore = score;
-            }
-        }
-        return maxScore;
-    }
 }
 
 
