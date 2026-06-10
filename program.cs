765ef4b327be48531f638daf568182c3f8ddca65
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-10 08:12:58 +0200

    reverse vowels of a string

--- program.cs
+++ program.cs
@@ -1,5 +1,39 @@
 public class Program
 {
+    public string ReverseVowels(string s)
+    {
+        var vowels = new HashSet<char>(){
+            'a', 'e', 'i', 'o', 'u'
+        };
+        int i = 0;
+        int j = s.Length - 1;
+        var sCharArray = s.ToCharArray();
+        while (i < j)
+        {
+            if (vowels.Contains(char.ToLower(s[i])) && vowels.Contains(char.ToLower(s[j])))
+            {
+                var temp = s[i];
+                sCharArray[i] = sCharArray[j];
+                sCharArray[j] = temp;
+                i++;
+                j--;
+            }
+            else if (!vowels.Contains(char.ToLower(s[i])) && !vowels.Contains(char.ToLower(s[j])))
+            {
+                i++;
+                j--;
+            }
+            else if (!vowels.Contains(char.ToLower(s[i])))
+            {
+                i++;
+            }
+            else if (!vowels.Contains(char.ToLower(s[j])))
+            {
+                j--;
+            }
+        }
+        return new String(sCharArray);
+    }
 }
 
 
