bd34157b14f1cbf14d11fbc3fc05701e1d92a513
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-15 08:37:37 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,36 +1,5 @@
 public class Program
 {
-    public bool IsHappy(int n)
-    {
-        n = GetNewN(n);
-        int map = new HashSet<int>();
-        while (n != 1)
-        {
-            n = GetNewN(n);
-            if (map.Contains(n))
-            {
-                return false;
-            }
-            else
-            {
-                map.Add(n);
-            }
-        }
-        return true;
-    }
-
-    private int GetNewN(int n)
-    {
-        string sN = n.ToString();
-        int newN = 0;
-        for (var i = 0; i < sN.Length; i++)
-        {
-            int x = int.Parse(sN[i].ToString());
-            int square = x * x;
-            newN += square;
-        }
-        return newN;
-    }
 }
 
 
