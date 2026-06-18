1000d3ff92b5ddb8a31ecc7b2c1c2c50ed61356e
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-18 19:12:57 +0200

    length of longest substring without repeating characters attempt 1

--- program.cs
+++ program.cs
@@ -1,5 +1,28 @@
 public class Program
 {
+    public int LengthOfLongestSubstring(string s)
+    {
+        var map = new HashSet<char>();
+        int count = 0;
+        int maxLength = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (map.Contains(s[i]))
+            {
+                count = 1;
+            }
+            else
+            {
+                map.Add(s[i]);
+                count++;
+                if (count > maxLength)
+                {
+                    maxLength = count;
+                }
+            }
+        }
+        return maxLength;
+    }
 }
 
 
