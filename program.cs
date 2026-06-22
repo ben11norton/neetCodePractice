24104a3441a74120176692920378077a581e769f
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-22 08:11:28 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,27 +1,5 @@
 public class Program
 {
-    public int MajorityElement(int[] nums)
-    {
-        // boyer moore algorithm
-        int candidate = 0;
-        int count = 0;
-        for (int i = 0; i < nums.Length; i++)
-        {
-            if (count == 0)
-            {
-                candidate = nums[i];
-            }
-            if (candidate == nums[i])
-            {
-                count++;
-            }
-            else
-            {
-                count--;
-            }
-        }
-        return candidate;
-    }
 }
 
 
