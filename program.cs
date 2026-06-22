81676563c53a43a2f77c3983965206888b071f99
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-22 07:40:57 +0200

    majority element with frequency map

--- program.cs
+++ program.cs
@@ -1,5 +1,31 @@
 public class Program
 {
+    public int MajorityElement(int[] nums)
+    {
+        var map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (map.ContainsKey(nums[i]))
+            {
+                map[nums[i]]++;
+            }
+            else
+            {
+                map[nums[i]] = 1;
+            }
+        }
+        int maxCount = 0;
+        int majorityElement = 0;
+        foreach (var (key, value) in map)
+        {
+            if (value > maxCount)
+            {
+                maxCount = value;
+                majorityElement = key;
+            }
+        }
+        return majorityElement;
+    }
 }
 
 
