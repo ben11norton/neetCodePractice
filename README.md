96da93b54568567f72d22dd9573b72713752a1e7
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-23 06:20:51 +0200

    word pattern

--- README.md
+++ README.md
@@ -168,6 +168,16 @@
 
 ![Happy Number](solutionDiagrams/happyNumber.jpg)
 
+<h1>Word Pattern</h1>
+
+<p>
+  <img src="https://img.shields.io/badge/Topics-ArrayHashing-indigo" />
+  <img src="https://img.shields.io/badge/Difficulty-Medium-orange" />
+  <img src="https://img.shields.io/badge/Status-Solved-success" />
+</p>
+
+![Word Pattern](solutionDiagrams/wordPattern.jpg)
+
 <h1>Remove duplicates from sorted array</h1>
 
 <p>
--- program.cs
+++ program.cs
@@ -1,5 +1,41 @@
 public class Program
 {
+    public bool WordPattern(string pattern, string s)
+    {
+        var patternMap = new Dictionary<char, string>();
+        var wordMap = new Dictionary<string, char>();
+        var sArray = s.Split(' ');
+        if (pattern.Length != sArray.Length)
+        {
+            return false;
+        }
+        for (var i = 0; i < pattern.Length; i++)
+        {
+            if (!patternMap.ContainsKey(pattern[i]))
+            {
+                patternMap[pattern[i]] = sArray[i];
+            }
+            else
+            {
+                if (sArray[i] != patternMap[pattern[i]])
+                {
+                    return false;
+                }
+            }
+            if (!wordMap.ContainsKey(sArray[i]))
+            {
+                wordMap[sArray[i]] = pattern[i];
+            }
+            else
+            {
+                if (pattern[i] != wordMap[sArray[i]])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
 
 
