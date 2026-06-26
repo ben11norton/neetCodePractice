071ebfe91efec646482dd6b16c7d2dc817694bc3
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-26 09:02:05 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,22 +1,5 @@
 public class Program
 {
-    public int RemoveDuplicates(int[] nums)
-    {
-        if (nums.Length <= 2)
-        {
-            return nums.Length;
-        }
-        int k = 2;
-        for (int i = 2; i < nums.Length; i++)
-        {
-            if (nums[i] != nums[k - 2])
-            {
-                nums[k] = nums[i];
-                k++;
-            }
-        }
-        return k;
-    }
 }
 
 
