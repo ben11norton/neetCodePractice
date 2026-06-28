e66c76d6ca361b7c5fac5f2623d820540ceacdb5
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-28 08:10:47 +0200

    word pattern overview

--- program.cs
+++ program.cs
@@ -1,5 +1,41 @@
 public class Program
 {
+    public bool WordPattern(string pattern, string s)
+    {
+        var sMap = new Dictionary<string, char>();
+        var pMap = new Dictionary<char, string>();
+        var sArray = s.Split(' ');
+        if (pattern.Length != sArray.Length)
+        {
+            return false;
+        }
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            if (!pMap.ContainsKey(pattern[i]))
+            {
+                pMap[pattern[i]] = sArray[i];
+            }
+            else
+            {
+                if (pMap[pattern[i]] != sArray[i])
+                {
+                    return false;
+                }
+            }
+            if (!sMap.ContainsKey(sArray[i]))
+            {
+                sMap[sArray[i]] = pattern[i];
+            }
+            else
+            {
+                if (sMap[sArray[i]] != pattern[i])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
 
 
