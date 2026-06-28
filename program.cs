9e004f961288800a2fb78a51e1589a578d2b9b6c
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-28 08:06:50 +0200

    majority element overview

--- program.cs
+++ program.cs
@@ -1,5 +1,27 @@
 public class Program
 {
+    public int MajorityElement(int[] nums)
+    {
+        int number = nums[0];
+        int half = nums.Length / 2;
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (count == 0)
+            {
+                number = nums[i];
+            }
+            if (number == nums[i])
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return number;
+    }
 }
 
 
