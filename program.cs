7662ba4aa5d353587d093e7f1e750fd8930e5a12
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-28 08:14:02 +0200

    remove duplicates from sorted array II

--- program.cs
+++ program.cs
@@ -1,5 +1,22 @@
 public class Program
 {
+    public int RemoveDuplicates(int[] nums)
+    {
+        int k = 2;
+        if (nums.Length <= 2)
+        {
+            return nums.Length;
+        }
+        for (int i = 2; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[k - 2])
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
 }
 
 
