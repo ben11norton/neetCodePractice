b5bc3aca4d360b294238bb4c42e53a1eb78e44b0
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-29 07:37:03 +0200

    next greater element 1

--- program.cs
+++ program.cs
@@ -1,5 +1,31 @@
 public class Program
 {
+    public int[] NextGreaterElement(int[] nums1, int[] nums2)
+    {
+        var stack = new Stack<int>();
+        var map = new Dictionary<int, int>();
+        for (int i = 0; i < nums2.Length; i++)
+        {
+            while (stack.Count > 0 && nums2[i] > stack.Peek())
+            {
+                map[stack.Pop()] = nums2[i];
+            }
+            stack.Push(nums2[i]);
+        }
+        var ans = new int[nums1.Length];
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            if (map.ContainsKey(nums1[i]))
+            {
+                ans[i] = map[nums1[i]];
+            }
+            else
+            {
+                ans[i] = -1;
+            }
+        }
+        return ans;
+    }
 }
 
 
