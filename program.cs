77c9c58d0f8f9400442c34417bd968af0846a41b
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-30 07:21:36 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,17 +1,5 @@
 public class Program
 {
-    public int FindNumbers(int[] nums)
-    {
-        int evenCount = 0;
-        for (int i = 0; i < nums.Length; i++)
-        {
-            if (nums[i].ToString().Length % 2 == 0)
-            {
-                evenCount++;
-            }
-        }
-        return evenCount;
-    }
 }
 
 
