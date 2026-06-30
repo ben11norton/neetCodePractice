c03b69c56f856b20fcc62b1083a1f3abf9b70e36
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-06-30 07:16:59 +0200

    find numbers with even number of digits

--- program.cs
+++ program.cs
@@ -1,5 +1,17 @@
 public class Program
 {
+    public int FindNumbers(int[] nums)
+    {
+        int evenCount = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i].ToString().Length % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+        return evenCount;
+    }
 }
 
 
