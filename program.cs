e35cdd3b858c5176cf8c062dc746fdf87bf7f666
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-07-06 17:14:49 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,27 +1,5 @@
 public class Program
 {
-    public int FindContentChildren(int[] g, int[] s)
-    {
-        Array.Sort(g);
-        Array.Sort(s);
-        int i = 0;
-        int j = 0;
-        int count = 0;
-        while (i < g.Length && j < s.Length)
-        {
-            if (s[j] >= g[i])
-            {
-                i++;
-                j++;
-                count++;
-            }
-            else if (s[j] < g[i])
-            {
-                j++;
-            }
-        }
-        return count;
-    }
 }
 
 
