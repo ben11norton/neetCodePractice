b25a846230910a48f8634f29ab5c73e73c46aaf0
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-07-06 07:58:34 +0200

    coming up with greedy solution for assing cookies

--- program.cs
+++ program.cs
@@ -1,5 +1,54 @@
 public class Program
 {
+    public int FindContentChildren(int[] g, int[] s)
+    {
+        g = checkAscOrder(g);
+        s = checkAscOrder(s);
+        int j = 0;
+        int count = 0;
+        for (int i = 0; i < g.Length; i++)
+        {
+            if (j <= s.Length - 1)
+            {
+                if (s[j] >= g[i])
+                {
+                    count++;
+                    j++;
+                }
+                else
+                {
+                    while (j < s.Length - 1 && s[j] <= g[i])
+                    {
+                        j++;
+                    }
+                    if (s[j] >= g[i])
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    private int[] checkAscOrder(int[] inputArray)
+    {
+        if (inputArray.Length > 1)
+        {
+            if (inputArray[0] > inputArray[1])
+            {
+                var sorted = new int[inputArray.Length];
+                int j = 0;
+                for (int i = inputArray.Length - 1; i >= 0; i--)
+                {
+                    sorted[j] = inputArray[i];
+                    j++;
+                }
+                return sorted;
+            }
+        }
+        return inputArray;
+    }
 }
 
 
