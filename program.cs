9d5ab8dd0e1fd8f22b32007c94c10b4d46927dfa
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-07-07 07:15:01 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,38 +1,5 @@
 public class Program
 {
-    public string RemoveStars(string s)
-    {
-        var stack = new Stack<char>();
-        for (int i = 0; i < s.Length; i++)
-        {
-            if (s[i] == '*')
-            {
-                if (stack.Count > 0)
-                {
-                    stack.Pop();
-                }
-            }
-            else
-            {
-                stack.Push(s[i]);
-            }
-        }
-        if (stack.Count > 0)
-        {
-            var sArray = new char[stack.Count];
-            int index = stack.Count - 1;
-            foreach (var item in stack)
-            {
-                sArray[index] = item;
-                index--;
-            }
-            return new String(sArray);
-        }
-        else
-        {
-            return "";
-        }
-    }
 }
 
 
