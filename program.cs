0c8927cc4a5fdcdf3347fec44423d80b608b28bd
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-07-09 08:31:09 +0200

    thrid maximum number

--- program.cs
+++ program.cs
@@ -1,5 +1,34 @@
 public class Program
 {
+    public int ThirdMax(int[] nums)
+    {
+        int? max = null;
+        int? secondMax = null;
+        int? thirdMax = null;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == max || nums[i] == secondMax || nums[i] == thirdMax)
+            {
+                // skip duplicate numbers
+            }
+            else if (max == null || nums[i] > max)
+            {
+                thirdMax = secondMax;
+                secondMax = max;
+                max = nums[i];
+            }
+            else if (secondMax == null || nums[i] > secondMax)
+            {
+                thirdMax = secondMax;
+                secondMax = nums[i];
+            }
+            else if (thirdMax == null || nums[i] > thirdMax)
+            {
+                thirdMax = nums[i];
+            }
+        }
+        return thirdMax != null ? (int)thirdMax : (int)max;
+    }
 }
 
 
