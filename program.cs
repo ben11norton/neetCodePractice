e0cd5888977a66839162cd9d7b9851393342e5ba
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-04 07:10:00 +0200

    2sum practice

--- program.cs
+++ program.cs
@@ -1,5 +1,23 @@
 public class Program
 {
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var sNum = target - nums[i];
+            if (map.ContainsKey(sNum))
+            {
+                var sNumIndex = map[sNum];
+                return [sNumIndex, i];
+            }
+            else
+            {
+                map[nums[i]] = i;
+            }
+        }
+        return new int[0];
+    }
 }
 
 
