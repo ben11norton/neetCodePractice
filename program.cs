8172c32e6164284a68db7d09d4bd91b3d1a53f37
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-06 07:44:55 +0200

    valid parenthesis

--- program.cs
+++ program.cs
@@ -1,5 +1,42 @@
 public class Program
 {
+    public bool IsValid(string s)
+    {
+        var openingMap = new Dictionary<char, char>(){
+            {'(', ')'},
+            {'[', ']'},
+            {'{', '}'},
+        };
+        var stack = new Stack<char>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (openingMap.ContainsKey(s[i]))
+            {
+                stack.Push(s[i]);
+            }
+            else if (stack.Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                var lastOpening = stack.Pop();
+                var bracketToMatch = openingMap[lastOpening];
+                if (s[i] != bracketToMatch)
+                {
+                    return false;
+                }
+            }
+        }
+        if (stack.Count > 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 }
 
 
