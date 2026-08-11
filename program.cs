e9e501810dd9f3487ac05b7407b7d0a62bcd1472
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-11 08:22:34 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,19 +1,5 @@
 public class Program
 {
-    public int ClimbStairs(int n)
-    {
-        var waysMap = new Dictionary<int, int>(){
-            {1, 1},
-            {2, 2}
-        };
-        for (int i = 3; i < n + 1; i++)
-        {
-            var ways = waysMap[i - 1] + waysMap[i - 2];
-            waysMap[i] = ways;
-        }
-        var waysN = waysMap[n];
-        return waysN;
-    }
 }
 
 
