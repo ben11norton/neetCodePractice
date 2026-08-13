32446e55c5216148697602f39e374be010a1e92e
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-13 07:05:32 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,34 +1,5 @@
 public class Program
 {
-    public bool IsPalindrome(string s)
-    {
-        int leftPointer = 0;
-        int rightPointer = s.Length - 1;
-        while (leftPointer != rightPointer && leftPointer < s.Length)
-        {
-            if (char.IsLetterOrDigit(s[leftPointer]) && char.IsLetterOrDigit(s[rightPointer]))
-            {
-                if (char.ToLower(s[leftPointer]) == char.ToLower(s[rightPointer]))
-                {
-                    leftPointer++;
-                    rightPointer--;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else if (!char.IsLetterOrDigit(s[leftPointer]))
-            {
-                leftPointer++;
-            }
-            else if (!char.IsLetterOrDigit(s[rightPointer]))
-            {
-                rightPointer--;
-            }
-        }
-        return true;
-    }
 }
 
 
