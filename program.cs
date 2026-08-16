5395b5312b031819c4c13ab2e2e53a7bb525a889
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-16 15:26:58 +0200

    best time to buy and sell stock comparing adjacent elements

--- program.cs
+++ program.cs
@@ -1,5 +1,30 @@
 public class Program
 {
+    public int MaxProfit(int[] prices)
+    {
+        int bestBuy = 0;
+        bool bestBuyAssigned = false;
+        int maxProfit = 0;
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (prices[i - 1] < prices[i])
+            {
+                int potentialBuy = prices[i - 1];
+                if (potentialBuy < bestBuy || !bestBuyAssigned)
+                {
+                    bestBuy = potentialBuy;
+                    bestBuyAssigned = true;
+                }
+                int potentialSell = prices[i];
+                int potentialProfit = potentialSell - bestBuy;
+                if (potentialProfit > maxProfit)
+                {
+                    maxProfit = potentialProfit;
+                }
+            }
+        }
+        return maxProfit;
+    }
 }
 
 
