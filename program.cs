488c9d50171db44fef37c6a42bc6d39b3211d6d0
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-17 08:01:36 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,23 +1,5 @@
 public class Program
 {
-    public bool HasCycle(ListNode head)
-    {
-        var hashSet = new HashSet<ListNode>();
-        var current = head;
-        while (current != null)
-        {
-            if (!hashSet.Contains(current))
-            {
-                hashSet.Add(current);
-            }
-            else
-            {
-                return true;
-            }
-            current = current.next;
-        }
-        return false;
-    }
 }
 
 
