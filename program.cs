a027d11aed2c7477810a07a79ace06d27d277728
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-17 07:44:23 +0200

    linked list cycle using hashset to detect if listnode has already been seen

--- program.cs
+++ program.cs
@@ -1,5 +1,23 @@
 public class Program
 {
+    public bool HasCycle(ListNode head)
+    {
+        var hashSet = new HashSet<ListNode>();
+        var current = head;
+        while (current != null)
+        {
+            if (!hashSet.Contains(current))
+            {
+                hashSet.Add(current);
+            }
+            else
+            {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
 }
 
 
