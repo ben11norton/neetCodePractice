132320f646fe4b422c8361f546ced896e4c96c05
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-18 07:30:14 +0200

    reverse bits and learning about 32 bit assigned integers + counting in binary

--- program.cs
+++ program.cs
@@ -1,5 +1,23 @@
 public class Program
 {
+    public int ReverseBits(int n)
+    {
+        string binaryString = Convert.ToString(n, 2).PadLeft(32, '0');
+        var binaryStringArray = binaryString.ToCharArray();
+        int l = 0;
+        int r = binaryStringArray.Length - 1;
+        while (l < r)
+        {
+            var swap = binaryStringArray[l];
+            binaryStringArray[l] = binaryStringArray[r];
+            binaryStringArray[r] = swap;
+            l++;
+            r--;
+        }
+        binaryString = new string(binaryStringArray);
+        int reverseBits = Convert.ToInt32(binaryString, 2);
+        return reverseBits;
+    }
 }
 
 
