fe2dfc8e2eb64a27b5dd967896476fc38dce6b1f
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-21 07:53:43 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,15 +1,5 @@
 public class Program
 {
-    public int HammingWeight(int n)
-    {
-        int setBits = 0;
-        while (n > 0)
-        {
-            n = n & (n - 1);
-            setBits++;
-        }
-        return setBits;
-    }
 }
 
 
