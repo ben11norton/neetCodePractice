c887886c1664bf064994c442d624d169f978c77d
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-21 07:53:21 +0200

    number of 1 bits using bit manipulation

--- program.cs
+++ program.cs
@@ -2,16 +2,11 @@ public class Program
 {
     public int HammingWeight(int n)
     {
-        // 1. brute force conerting n to binary string
-        // and counting 1's
-        string binaryN = Convert.ToString(n, 2);
         int setBits = 0;
-        for (int i = 0; i < binaryN.Length; i++)
+        while (n > 0)
         {
-            if (binaryN[i] == '1')
-            {
-                setBits++;
-            }
+            n = n & (n - 1);
+            setBits++;
         }
         return setBits;
     }
