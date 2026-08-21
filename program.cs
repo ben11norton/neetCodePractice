5fc22be9c528b1971918984c2fc3db94187424e5
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-21 07:52:47 +0200

    brute force set bits through counting 1s

--- program.cs
+++ program.cs
@@ -1,5 +1,20 @@
 public class Program
 {
+    public int HammingWeight(int n)
+    {
+        // 1. brute force conerting n to binary string
+        // and counting 1's
+        string binaryN = Convert.ToString(n, 2);
+        int setBits = 0;
+        for (int i = 0; i < binaryN.Length; i++)
+        {
+            if (binaryN[i] == '1')
+            {
+                setBits++;
+            }
+        }
+        return setBits;
+    }
 }
 
 
