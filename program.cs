53e1a45f52ab817e7d1dcda41599accfc7258107
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-24 18:07:21 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,21 +1,5 @@
 public class Program
 {
-    public bool ContainsDuplicate(int[] nums)
-    {
-        var map = new HashSet<int>();
-        for (int i = 0; i < nums.Length; i++)
-        {
-            if (!map.Contains(nums[i]))
-            {
-                map.Add(nums[i]);
-            }
-            else
-            {
-                return true;
-            }
-        }
-        return false;
-    }
 }
 
 
