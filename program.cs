4c369b76abb958cb288e37d5dd57efcbd9a51077
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-24 18:07:04 +0200

    contains duplicate using hashset for O(n) look up

--- program.cs
+++ program.cs
@@ -1,5 +1,21 @@
 public class Program
 {
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var map = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!map.Contains(nums[i]))
+            {
+                map.Add(nums[i]);
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 
