85c1314008390b828cf418df217aae76015c9284
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-25 08:23:48 +0200

    reverse linked list

--- program.cs
+++ program.cs
@@ -1,5 +1,39 @@
 public class Program
 {
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode nextNode = null;
+        var current = head;
+        var p = current;
+        while (current != null)
+        {
+            if (nextNode == null)
+            {
+                current = current.next;
+                p.next = null;
+            }
+            if (current != null)
+            {
+                nextNode = current.next;
+                current.next = p;
+            }
+            if (nextNode != null)
+            {
+                p = current;
+                current = nextNode;
+            }
+            else if (current != null)
+            {
+                head = current;
+                current = nextNode;
+            }
+            else
+            {
+                head = p;
+            }
+        }
+        return head;
+    }
 }
 
 
