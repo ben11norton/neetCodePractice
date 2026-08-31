40b148c4235676e7c3c21d8aa2a6541091a9f985
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-08-31 07:39:30 +0200

    missing number

--- program.cs
+++ program.cs
@@ -1,5 +1,30 @@
 public class Program
 {
+    public int MissingNumber(int[] nums)
+    {
+        int n = nums.Length;
+        var map = new Dictionary<int, bool>();
+        for (int i = 0; i <= n; i++)
+        {
+            map[i] = false;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (map.ContainsKey(nums[i]))
+            {
+                map[nums[i]] = true;
+            }
+        }
+        int missingNumber = 0;
+        foreach (var (key, value) in map)
+        {
+            if (value == false)
+            {
+                missingNumber = key;
+            }
+        }
+        return missingNumber;
+    }
 }
 
 
