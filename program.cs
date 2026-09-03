9e9a925bd9f22a5cd4fcef9da775c186885a6be0
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-09-03 08:07:02 +0200

    Counting Bits using Counting 1s binary trick n & n-1

--- program.cs
+++ program.cs
@@ -1,5 +1,21 @@
 public class Program
 {
+    public int[] CountBits(int n)
+    {
+        var ans = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            int number1Count = 0;
+            int number = i;
+            while (number != 0)
+            {
+                number = number & number - 1;
+                number1Count++;
+            }
+            ans[i] = number1Count;
+        }
+        return ans;
+    }
 }
 
 
