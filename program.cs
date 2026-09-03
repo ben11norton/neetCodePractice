7c958b6ff9195d4665f66ecf028e268bbeac4650
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2026-09-03 08:03:21 +0200

    clean

--- program.cs
+++ program.cs
@@ -1,28 +1,5 @@
 public class Program
 {
-    public int[] CountBits(int n)
-    {
-        var binaryNArray = new string[n + 1];
-        for (int i = 0; i <= n; i++)
-        {
-            string binaryN = Convert.ToString(i, 2);
-            binaryNArray[i] = binaryN;
-        }
-        var ans = new int[n + 1];
-        for (int i = 0; i <= n; i++)
-        {
-            int number1Count = 0;
-            foreach (var binaryRep in binaryNArray[i])
-            {
-                if (binaryRep == '1')
-                {
-                    number1Count++;
-                }
-            }
-            ans[i] = number1Count;
-        }
-        return ans;
-    }
 }
 
 
